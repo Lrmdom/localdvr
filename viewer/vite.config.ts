80e6bcf3604c00484c8bf740ac5739079d2323f9
Lrmdom <lmatiasdomingos@gmail.com>
2026-08-10 23:46:13 +0100

    it is working in all networks including mobile

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
     proxy: {
-      '/api': 'http://127.0.0.1:8000'
+      '/api': {
+        target: 'http://recorder:8000', // Nome do serviço no docker-compose
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
   build: {
