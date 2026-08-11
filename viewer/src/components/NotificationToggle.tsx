dd09f8de8fbaec8efbeee8bbd964130c218373d3
Lrmdom <lmatiasdomingos@gmail.com>
2026-08-11 01:11:29 +0100

    save work

--- viewer/src/components/NotificationToggle.tsx
+++ viewer/src/components/NotificationToggle.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export const NotificationToggle = () => {
   const [permission, setPermission] = useState(Notification.permission);
